24a248f98eb8d6e0e3a1b0a2bd92c814099e5113
BJ Pennington <bjpennington112@gmail.com>
2021-06-05 23:28:59 -0500

    load data from endpoint for meeting rooms

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import MeetingRoomList from './MeetingRoomList';
 
 function App() {
+  const [rooms, setRooms] = useState([]);
+  const [loadError, setLoadError] = useState(false);
+
+  useEffect(() => {
+    let isMounted = true;
+    axios.get('https://wetransfer.github.io/rooms.json')
+      .then(response => {
+        const returnedRooms = response.data.rooms;
+        if (isMounted) {
+          setRooms(returnedRooms);
+        }
+      })
+      .catch(error => {
+        console.error(error);
+        setLoadError(true);
+      })
+      return () => { isMounted = false };
+  }, []);
+
+  const loadErrorMessage = () => {
+    return <p>Failed to load rooms. Please try again later.</p>
+  }
+
   return (
     <>
       <h1>Rooms</h1>
       <p>The number of available slots are shown for each room. Click "book" to book a room immediately.</p>
+      { loadError ? loadErrorMessage() : <MeetingRoomList rooms={rooms} /> }
     </>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,30 @@
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
 import App from './App';
+
+const sampleRooms = {
+  rooms: [{
+    name: "Mears Park",
+    spots: 8,
+    thumbnail: null
+  },
+  {
+    name: "Rice Park",
+    spots: 0,
+    thumbnail: null
+  }]
+};
+
+const server = setupServer(
+  rest.get('https://wetransfer.github.io/rooms.json', (req, res, ctx) => {
+    return res(ctx.json(sampleRooms))
+  })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 test('it renders title', () => {
   const { getByText} = render(<App />);
@@ -10,4 +35,30 @@ test('it renders intro text', () => {
   const { getByText } = render (<App />);
   const introText = 'The number of available slots are shown for each room. Click "book" to book a room immediately.';
   expect(getByText(introText)).toBeInTheDocument();
-})
+});
+
+test('it requests meeting rooms', async() => {
+  const { getByText, queryByText } = render(<App />);
+  expect(getByText('No rooms available. Please try again later.')).toBeInTheDocument();
+  expect(queryByText(sampleRooms.rooms[0].name)).not.toBeInTheDocument();
+  await waitFor(() => { expect(getByText(sampleRooms.rooms[0].name)).toBeInTheDocument() });
+  expect(queryByText('No rooms available. Please try again later.')).not.toBeInTheDocument();
+});
+
+
+test('handlers server error', async () => {
+  // suppress expected error to avoid confusing test output
+  console.error = jest.fn();
+  server.use(
+    rest.get('https://wetransfer.github.io/rooms.json', (req, res, ctx) => {
+      return res(ctx.status(500))
+    })
+  );
+  const { getByText, queryByText } = render(<App />);
+  expect(getByText('No rooms available. Please try again later.')).toBeInTheDocument();
+  expect(queryByText('Failed to load rooms. Please try again later.')).not.toBeInTheDocument();
+
+  await waitFor(() => { expect(getByText('Failed to load rooms. Please try again later.')).toBeInTheDocument() });
+  expect(queryByText('No rooms available. Please try again later.')).not.toBeInTheDocument();
+  expect(console.error).toHaveBeenCalled();
+});
