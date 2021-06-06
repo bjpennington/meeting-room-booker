d9d15dd1feec6ec4d01e8cab641f3c51f0d174e9
BJ Pennington <bjpennington112@gmail.com>
2021-06-06 12:43:48 -0500

    add sweet alert and complete booking behavior

--- src/MeetingRoom.js
+++ src/MeetingRoom.js
@@ -1,15 +1,42 @@
 import { useState } from 'react'
+import swal from 'sweetalert';
 import defaultImage from './default_room.png';
 import BookButton from './BookButton';
+import axios from 'axios';
 
 const MeetingRoom = ({ room }) => {
   const [spots, setSpots] = useState(room.spots);
 
   const bookRoom = () => {
     if(spots && spots > 0) {
-      setSpots(spots - 1);
+      axios.get('https://wetransfer.github.io/bookRoom.json')
+        .then(response => {
+          if(response.data.success) {
+            bookingSuccess();
+          }
+          else bookingFailure();
+        })
+        .catch(error => {
+          console.error(error);
+          bookingFailure();
+        })
     }
   };
+
+  const bookingSuccess = () => {
+    swal({
+      title: "Successfully booked",
+      icon: "success"
+    });
+    setSpots(spots - 1);
+  }
+
+  const bookingFailure = () => {
+    swal({
+      title: "Error: failed to book",
+      icon: "error"
+    });
+  }
 
   const spotsAvailable = (spots) => {
     if(spots && spots > 0) {
@@ -28,7 +55,10 @@ const MeetingRoom = ({ room }) => {
         src={room.thumbnail ? room.thumbnail : defaultImage }
       />
       <p>{spotsAvailable(spots)}</p>
-      <BookButton bookRoom={bookRoom} />
+      <BookButton
+        bookable={spots && spots > 0}
+        bookRoom={bookRoom}
+      />
     </>
   )
 };
--- src/MeetingRoom.test.js
+++ src/MeetingRoom.test.js
@@ -1,4 +1,6 @@
 import { render, waitFor } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 import userEvent from '@testing-library/user-event';
 import MeetingRoom from './MeetingRoom';
 import logoThumbnail from '../public/logo512.png';
@@ -14,6 +16,16 @@ const badDataRoom = {
   spots: null,
   thumbnail: null
 };
+
+const server = setupServer(
+  rest.get('https://wetransfer.github.io/bookRoom.json', (req, res, ctx) => {
+    return res(ctx.json({success: true}))
+  })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 test('it displays a room name and number of spots', () => {
   const { getByText } = render(<MeetingRoom room={sampleRoom} />);
@@ -43,11 +55,27 @@ test('it displays default image if no thumbnail included', () => {
 
 test('book button is present', () => {
   const { getByRole } = render(<MeetingRoom room={sampleRoom} />);
-  expect(getByRole('button')).toBeInTheDocument();
+  expect(getByRole('button', { name: 'Book!' })).toBeInTheDocument();
 });
 
 test('clicking book button books room and updates available slots', async() => {
   const { getByRole, getByText } = render(<MeetingRoom room={sampleRoom} />);
-  userEvent.click(getByRole('button'));
-  await waitFor(() => expect(getByText('7 spots remaining')).toBeInTheDocument());
+  userEvent.click(getByRole('button', { name: 'Book!' }));
+  await waitFor(() => expect(getByText('Successfully booked')).toBeInTheDocument());
+  expect(getByText('7 spots remaining')).toBeInTheDocument();
+});
+
+test('unsuccessful booking presents alert', async() => {
+  // suppress expected error to avoid confusing test output
+  console.error = jest.fn();
+  server.use(
+    rest.get('https://wetransfer.github.io/bookRoom.json', (req, res, ctx) => {
+      return res(ctx.status(500))
+    })
+  );
+  const { getByRole, getByText } = render(<MeetingRoom room={sampleRoom} />);
+  userEvent.click(getByRole('button', { name: 'Book!' }));
+  await waitFor(() => expect(getByText('Error: failed to book')).toBeInTheDocument());
+  expect(getByText(`${sampleRoom.spots} spots remaining`)).toBeInTheDocument();
+  expect(console.error).toHaveBeenCalled();
 });
