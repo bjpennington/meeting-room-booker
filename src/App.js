e0b5905439318228ed68ba33aa9519235edb2eeb
BJ Pennington <bjpennington112@gmail.com>
2021-06-05 19:30:11 -0500

    remove react boilerplate and add meeting room title and intro text

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Rooms</h1>
+      <p>The number of available slots are shown for each room. Click "book" to book a room immediately.</p>
+    </>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByText('Rooms');
+  expect(title).toBeInTheDocument();
 });
+
+test('renders intro text', () => {
+  render (<App />);
+  const introText = screen.getByText('The number of available slots are shown for each room. Click "book" to book a room immediately.');
+  expect(introText).toBeInTheDocument();
+})
